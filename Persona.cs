4252af74ff1645a9c5b0bae2979d0a7bf577a80a
karlabarrios <barrioskarla154@gmail.com>
2023-09-20 07:35:23 -0600

    practica resuelta

---
+++ Persona.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioExtra
+{
+    class Persona
+    {
+        //Persona: nombre, apellido, fecha de nacimiento, numeroTelefono, correo
+
+        public string nombre { get; }
+        public string apellido { get; }
+        public int fechaNacimiento { get; }
+        private string numeroTelefono;
+        private string correo;
+        private int edad;
+        public string nacionalidad { get; }
+
+        public Persona(string nombre, string apellido, int fechaNacimiento)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.fechaNacimiento = fechaNacimiento;
+            nacionalidad = "mexicano(a)";
+            edad = 2023 - fechaNacimiento;
+        }
+        public int GetEdad()
+        {
+            return edad;
+        }
+
+        public string NumeroTelefono
+        {
+            get { return numeroTelefono; }
+            set
+            {
+                if (value.Length == 10)
+                    numeroTelefono = value;
+                else
+                    numeroTelefono = "Su telefono no tiene la cantidad de valores correcta";
+            }
+        }
+        public string Correo
+        {
+            get { return correo; }
+            set
+            {
+                if (value.Contains("@gmail.com"))
+                    correo = value;
+                else
+                    correo = "No se ingreso un correo gmail";
+            }
+        }
+
+        public string darEdad()
+        {
+            return "Mi edad es: " + edad;
+        }
+
+        public string darEdad(string extraño)
+        {
+            return "Hola " + extraño + " mi edad es: " + edad;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioExtra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona per1 = new Persona("Nieve","Serena", 1993);
+            Console.WriteLine("Ingrese su numero telefónico:");
+            per1.NumeroTelefono = Console.ReadLine();
+            Console.WriteLine("Ingrese su correo electrónico:");
+            per1.Correo = Console.ReadLine();
+            Console.WriteLine(per1.darEdad("pao"));
+            Console.WriteLine("La nacionalidad de " + per1.nombre + " " + per1.apellido + " es " + per1.nacionalidad);
+            Console.WriteLine("La edad de " + per1.nombre + " " + per1.apellido + " es " + per1.GetEdad() + " años");
+            Console.WriteLine("Su número telefónico es: " + per1.NumeroTelefono);
+            Console.WriteLine("Su correo electrónico es: " + per1.Correo);
+
+
+            Console.ReadKey();
+        }
+    }
+}
